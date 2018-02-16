425256c662ad352f5f81d0a0cd7099613583b918
Unknown <marcandrea.hunkeler@student.unisg.ch>
2018-02-16 14:49:57 +0100

    This is the new Test!

--- Test_Production.py
+++ Test_Production.py
@@ -3,10 +3,20 @@ import production
 
 class TestProduction(unittest.TestCase):
 
+<<<<<<< Updated upstream
     def test_remove_duplicates(self):
         input = [1,2,2,1]
         output = [1,2]
         self.assertListEqual(output,production.remove_duplicates(input))
+=======
+    def square(self):
+
+        self.assertEqual(5,25)
+        self.assertEqual(0,0)
+        self.assertEqual(-2,'Error')
+        self.assertEqual('a','Error')
+
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     unittest.main()
