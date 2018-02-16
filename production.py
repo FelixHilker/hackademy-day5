23e4562a9927066b209e49f466a13cb6092edbd2
Unknown <marcandrea.hunkeler@student.unisg.ch>
2018-02-16 12:11:51 +0100

    Add production and test

---
+++ production.py
@@ -0,0 +1,11 @@
+def remove_dublicates(a_list):
+    new_list = []
+
+    for i in a_list:
+        for j in a_list:
+            if a_list[i] == a_list[j]
+                if i == j:
+                    continue
+                else:
+                    new_list.append(i)
+    return new_list
